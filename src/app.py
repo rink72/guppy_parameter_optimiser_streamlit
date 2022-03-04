5ee07899679a3b31891e092620e1478e0765bb6a
Sam <beardmans72@gmail.com>
2022-03-05 12:01:37 +1300

    Update to convert csv data on the fly

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+import helpers.converters as conv
 
 
 def GetBenchmarkData() -> pd.DataFrame:
@@ -8,10 +9,9 @@ def GetBenchmarkData() -> pd.DataFrame:
   # We will likely change this in the future to pull from
   # a URL or dataabase
   flatDataPath = os.path.join(os.path.dirname(__file__), '..', 'data', 'benchmark-data.csv')
-  typeDataPath = os.path.join(os.path.dirname(__file__), '..', 'data', 'benchmark-data.json')
 
   flatData = pd.read_csv(flatDataPath)
-  typeData = pd.read_json(typeDataPath)
+  typeData = conv.ConvertFlatToType(flatData=flatData)
   return flatData, typeData
 
 
---
+++ src/helpers/converters.py
@@ -0,0 +1,46 @@
+import os
+import csv
+import pandas as pd
+import json
+
+def ConvertStringToFloat(string: str) -> float:
+  try:
+    return float(string)
+  except:
+    return None
+
+
+# Convert the flat csv file to a JSON file
+# based on model. This is mainly for ease of
+# presenting the data
+
+def ConvertFlatToType(flatData: pd.DataFrame) -> pd.DataFrame:
+
+  typeData = list()
+
+  for r, row in flatData.iterrows():
+    typeData.append(
+      {
+        "Processor": row["Processor"],
+        "Model": "FAST",
+        "Samples": ConvertStringToFloat(row["FAST"])
+      }
+    )
+
+    typeData.append(
+      {
+        "Processor": row["Processor"],
+        "Model": "HAC",
+        "Samples": ConvertStringToFloat(row["HAC"])
+      }
+    )
+
+    typeData.append(
+      {
+        "Processor": row["Processor"],
+        "Model": "SUP",
+        "Samples": ConvertStringToFloat(row["SUP"])
+      }
+    )
+
+  return pd.DataFrame(typeData)
